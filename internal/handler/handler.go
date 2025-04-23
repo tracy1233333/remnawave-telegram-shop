5847829dcb6ad2e6c0611fc5934cd5863719d633
Jesus <valmak29@gmail.com>
2025-04-24 00:58:09 +0400

    feat: add fancy share button.

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -65,20 +65,26 @@ func (h Handler) ReferralCallbackHandler(ctx context.Context, b *bot.Bot, update
 	customer, _ := h.customerRepository.FindByTelegramId(ctx, update.CallbackQuery.From.ID)
 	langCode := update.CallbackQuery.From.LanguageCode
 	refCode := customer.TelegramID
-	refLink := fmt.Sprintf("https://t.me/%s?start=ref_%d", update.CallbackQuery.Message.Message.From.Username, refCode)
+
+	refLink := fmt.Sprintf("https://telegram.me/share/url?url=https://t.me/%s?start=ref_%d", update.CallbackQuery.Message.Message.From.Username, refCode)
 	count, err := h.referralRepository.CountByReferrer(ctx, customer.TelegramID)
 	if err != nil {
 		slog.Error("error counting referrals", err)
 	}
-	text := fmt.Sprintf(h.translation.GetText(langCode, "referral_text"), refLink, count)
+	text := fmt.Sprintf(h.translation.GetText(langCode, "referral_text"), count)
 	callbackMessage := update.CallbackQuery.Message.Message
 	_, err = b.EditMessageText(ctx, &bot.EditMessageTextParams{
 		ChatID:    callbackMessage.Chat.ID,
 		MessageID: callbackMessage.ID,
 		Text:      text,
-		ReplyMarkup: models.InlineKeyboardMarkup{InlineKeyboard: [][]models.InlineKeyboardButton{{
-			{Text: h.translation.GetText(langCode, "back_button"), CallbackData: CallbackStart},
-		}}},
+		ReplyMarkup: models.InlineKeyboardMarkup{InlineKeyboard: [][]models.InlineKeyboardButton{
+			{
+				{Text: h.translation.GetText(langCode, "share_referral_button"), URL: refLink},
+			},
+			{
+				{Text: h.translation.GetText(langCode, "back_button"), CallbackData: CallbackStart},
+			},
+		}},
 	})
 	if err != nil {
 		slog.Error("Error sending referral message", err)
