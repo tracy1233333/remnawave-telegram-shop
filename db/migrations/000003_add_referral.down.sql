88d76cff190df2d22962146556a2fe1ad637d6b1
Jesus <valmak29@gmail.com>
2025-04-23 22:34:40 +0400

    fix: resolve duplicates in tables.

--- db/migrations/000003_add_referral.down.sql
+++ db/migrations/000003_add_referral.down.sql
@@ -1,1 +1,11 @@
+BEGIN;
+
+DROP INDEX IF EXISTS idx_referral_referrer_id;
+DROP INDEX IF EXISTS idx_referral_referee_id;
+
 DROP TABLE IF EXISTS referral;
+
+ALTER TABLE customer
+DROP CONSTRAINT IF EXISTS customer_telegram_id_unique;
+
+COMMIT;
